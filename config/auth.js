dee1ce9fd3abf5523c2e2da60146c73f126d2428
Thais Martins <contato@thaismartins.co>
2018-11-17 18:54:43 -0200

    Adding auth and login with user model

--- config/auth.js
+++ config/auth.js
@@ -5,49 +5,44 @@ const LocalStrategy = require('passport-local').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const config = require('./settings');
 const passport = require('passport');
+const UserModel = require('../users/model');
 
-const options = {
+passport.use(new JwtStrategy({
   "jwtFromRequest": ExtractJwt.fromAuthHeaderAsBearerToken(),
   "secretOrKey": config.jwt.secret
-};
-
-passport.use(new JwtStrategy(options, (payload, done) => {
-  
-    // TODO: validation user
-    if (payload.user.id == 1234) {
-        return done(null, true)
+}, async (payload, done) => {
+  try {
+    const user = await UserModel.findOne({ _id: payload.user.id });
+    if(!user) {
+      const error = new Error('Credentials not found');
+      return done(error, false);
     }
 
-    return done(null, false)
+    return done(null, true);
+  } catch(error) {
+    return done(error, false);
+  }
 }));
 
-//Create a passport middleware to handle User login
 passport.use('login', new LocalStrategy({
     usernameField : 'email',
     passwordField : 'password'
-  }, (email, password, done) => {
+}, async (email, password, done) => {
+  try {
+    const user = await UserModel.findOne({ email });
+    if(!user) {
+      const error = new Error('User not found');
+      return done(error, false);
+    }
 
-    console.log('email', email);
-    console.log('password', password);
-    
-    return done(false, { id : 1234 });
+    const validate = await user.isCorrectPassword(password);
+    if(!validate) {
+      const error = new Error('Wrong Password');
+      return done(error, false);
+    }
 
-    // try {
-    //   //Find the user associated with the email provided by the user
-    //   const user = await UserModel.findOne({ email });
-    //   if( !user ){
-    //     //If the user isn't found in the database, return a message
-    //     return done(null, false, { message : 'User not found'});
-    //   }
-    //   //Validate password and make sure it matches with the corresponding hash stored in the database
-    //   //If the passwords match, it returns a value of true.
-    //   const validate = await user.isValidPassword(password);
-    //   if( !validate ){
-    //     return done(null, false, { message : 'Wrong Password'});
-    //   }
-    //   //Send the user information to the next middleware
-    //   return done(null, user, { message : 'Logged in Successfully'});
-    // } catch (error) {
-    //   return done(error);
-    // }
-  }));
+    return done(false, user);
+  } catch (error) {
+    return done(error, false);
+  }
+}));
